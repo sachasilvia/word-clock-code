c16f0720447ab55153a83fb8bf6cec0d25c38573
Sacha Silvia <149610520+sachasilvia@users.noreply.github.com>
2023-11-30 13:29:19 -0500

    Add files via upload

---
+++ To_and_Past_final.ino
@@ -0,0 +1,52 @@
+#include <PicoEspTime.h>
+#include <Adafruit_NeoPixel.h>
+#define PIN 6
+#define NUMPIXELS 129
+#define DELAYVAL 1000
+PicoEspTime rtc;
+uint32_t lastTime;
+int hour;
+int lasthour;
+Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+void setup() {
+  Serial.begin(115200);
+  rtc.adjust(23, 59, 55, 2023, 11, 16);
+  pixels.clear();                       
+  rtc.read();
+   for (int past = 67; past < 71; past++) {
+    if ((rtc.minute >= 5) && (rtc.minute <= 29)) {
+      pixels.setPixelColor(past, pixels.Color(255, 0, 0));
+    } else {
+      pixels.setPixelColor(past, pixels.Color(0, 0, 0));
+    }
+  }
+  for (int to = 66; to < 68; to++) {
+    if (rtc.minute >= 30) {
+      pixels.setPixelColor(to, pixels.Color(255, 0, 0));
+    } else {
+      pixels.setPixelColor(66, pixels.Color(0, 0, 0));
+    }
+  }
+  pixels.show();
+}
+void loop() {
+  if (millis() - lastTime >= 1000) {
+    rtc.read();
+    lastTime = millis();
+    for (int past = 67; past < 71; past++) {
+      if ((rtc.minute >= 5) && (rtc.minute <= 29)) {
+        pixels.setPixelColor(past, pixels.Color(255, 0, 0));
+      } else {
+        pixels.setPixelColor(past, pixels.Color(0, 0, 0));
+      }
+    }
+    for (int to = 66; to < 68; to++) {
+    if (rtc.minute >= 30) {
+      pixels.setPixelColor(to, pixels.Color(255, 0, 0));
+    } else {
+      pixels.setPixelColor(66, pixels.Color(0, 0, 0));
+    }
+  }
+    pixels.show();
+  }
+}
